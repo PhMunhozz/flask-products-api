3ce0abd26053f281ae1291f5003d8e98eda695b1
Paulo Munhoz <paulo.henriquemcf@gmail.com>
2025-04-13 14:10:02 -0300

    feat: add layered error handling for get products

--- app/repositories/product_repository.py
+++ app/repositories/product_repository.py
@@ -7,19 +7,22 @@ class ProductRepository:
     @staticmethod
     def get_products(name=None, category=None, barcode=None, price=None):
         filtered_products = []
+        
+        try:
+            for product in ProductRepository.products.values():
+                if name and name.lower() not in product.name.lower():
+                    continue
+                if category and category.lower() not in product.category.lower():
+                    continue
+                if barcode and barcode.lower() not in product.barcode.lower():
+                    continue
+                if price and price != product.price:
+                    continue
+                filtered_products.append(product)
 
-        for product in ProductRepository.products.values():
-            if name and name.lower() not in product.name.lower():
-                continue
-            if category and category.lower() not in product.category.lower():
-                continue
-            if barcode and barcode.lower() not in product.barcode.lower():
-                continue
-            if price and float(price) != product.price:
-                continue
-            filtered_products.append(product)
-
-        return filtered_products
+            return filtered_products
+        except DatabaseError:
+            raise
     
     @staticmethod
     def get_product_by_id(id: int):
